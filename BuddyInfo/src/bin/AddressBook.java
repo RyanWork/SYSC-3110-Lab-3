e4a29c6986ab2b5db91648824633d4bd9a0d3501
ryanha <ryanha@aa0508-64.labs.sce.carleton.ca>
2016-09-27 11:59:57 -0400

    hi

---
+++ BuddyInfo/src/bin/AddressBook.java
@@ -0,0 +1,28 @@
+package bin;
+
+import java.util.*;
+
+public class AddressBook {
+	private ArrayList<BuddyInfo> users;
+	
+	public AddressBook(){
+		this.users = new ArrayList<BuddyInfo>(); 
+	}
+	
+	public void addBuddy(BuddyInfo Buddy){
+		this.users.add(Buddy);
+	}
+	
+	public void removeBuddy(BuddyInfo Buddy){
+		users.remove(Buddy);
+	}
+	
+	public static void main (String args[]){
+		BuddyInfo buddy = new BuddyInfo();
+		buddy.setName("Daman");
+		buddy.setSize(1234);
+		AddressBook addressbook = new AddressBook();
+		addressbook.addBuddy(buddy);
+		addressbook.removeBuddy(buddy);
+	}
+}
---
+++ BuddyInfo/src/bin/BuddyInfo.java
@@ -0,0 +1,32 @@
+package bin;
+
+public class BuddyInfo {
+	private String name, address;
+	private int size;
+	
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public static void main(String[] args) {
+		BuddyInfo friend = new BuddyInfo();
+		friend.setName("Daman");
+		System.out.println(friend.getName());
+	}
+
+}
